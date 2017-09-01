646106e538729ade0ce7a3aee8bcbd30d28dffc6
yobmod <yobmod@gmail.com>
2017-09-01 23:29:08 +0100

    heroku deploy

--- src/dmlUtgard/wsgi.py
+++ src/dmlUtgard/wsgi.py
@@ -16,8 +16,11 @@ from whitenoise.django import DjangoWhiteNoise
 
 
 
+try:
+	dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+except:
+	pass
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmlUtgard.settings")
 
