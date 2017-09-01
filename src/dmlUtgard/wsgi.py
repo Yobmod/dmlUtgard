87043513ec19d856c9e368e224a2a7bae902e195
yobmod <yobmod@gmail.com>
2017-09-02 00:20:40 +0100

    heroku deploy

--- src/dmlUtgard/wsgi.py
+++ src/dmlUtgard/wsgi.py
@@ -13,16 +13,12 @@ import dotenv
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-
-
-
 try:
 	dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 except:
 	pass
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmlutgard.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmluttgard.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application) # , root='/path/to/static/files')
--- src/manage.py
+++ src/manage.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 		dotenv.read_dotenv()
 	except:
 		pass
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmlUtgard.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmlutgard.settings")
 	try:
 		from django.core.management import execute_from_command_line
 	except ImportError:
