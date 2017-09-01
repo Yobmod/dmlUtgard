53af3d57375dc24008b196446cb1a924dc2672db
Laventine <yobmod@gmail.com>
2017-09-01 17:54:02 +0100

    add cached file storage

--- src/dmlUtgard/production_settings.py
+++ src/dmlUtgard/production_settings.py
@@ -65,7 +65,7 @@ DEFAULT_FILE_STORAGE = 'dmlutgard.storages_custom.MediaStorage'
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-STATICFILES_STORAGE = 'dmlUtgard.storages_custom.StaticStorage'
+STATICFILES_STORAGE = 'dmlUtgard.storages_custom.CachedS3BotoStorage'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),	]
 
