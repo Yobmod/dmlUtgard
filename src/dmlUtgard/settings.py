ca2d219f78463dd93812d446b158265c19c247df
yobmod <yobmod@gmail.com>
2017-09-01 22:58:25 +0100

    deploy to heroku

--- src/dmlUtgard/settings.py
+++ src/dmlUtgard/settings.py
@@ -1,7 +1,7 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 
 INSTALLED_APPS = [
@@ -158,7 +158,7 @@ COMPRESS_JS_FILTERS = [
 # 	MEDIA_URL = "/media/"
 # 	MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
-if DEBUG == True:
+if DEBUG == False:
 	try:
 		from .production_settings import *
 		print(" * using production environment settings")
