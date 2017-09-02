c4ed9eb835724e7b0a6fc3f11e9e303bf2d0f181
yobmod <yobmod@gmail.com>
2017-09-02 01:30:17 +0100

    heroku deploy

--- dmlutgard/wsgi.py
+++ dmlutgard/wsgi.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 import dotenv
-from django.conf import settings
+#from django.conf import settings
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-if settings.DEBUG == True:
-	dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+#if settings.DEBUG == True:
+	#dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmlutgard.settings")
 
