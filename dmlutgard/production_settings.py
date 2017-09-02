9366311d89dada62a47997024deab69fa4406d2f
yobmod <yobmod@gmail.com>
2017-09-02 01:48:02 +0100

    heroku statics

--- dmlutgard/production_settings.py
+++ dmlutgard/production_settings.py
@@ -67,8 +67,8 @@ DEFAULT_FILE_STORAGE = 'dmlutgard.storages_custom.MediaStorage'
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-STATICFILES_STORAGE = 'dmlutgard.storages_custom.CachedS3BotoStorage'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
+STATICFILES_STORAGE = 'dmlutgard.storages_custom.StaticStorage'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "cdn/static_root")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),	]
 
 #STATICFILES_STORAGE ='dmlutgard.storages_custom.CachedS3BotoStorage'.
--- dmlutgard/settings.py
+++ dmlutgard/settings.py
@@ -166,7 +166,7 @@ if DEBUG == True:
 		print(" * using local environment settings")
 		pass
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "cdn/static_root")
 
 #WHITENOISE_AUTOREFRESH # =DEBUG
 WHITENOISE_ROOT = STATIC_ROOT
