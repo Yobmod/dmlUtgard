61913e8293fc0ad0047b2a4f9116f8c6a9e38050
Laventine <yobmod@gmail.com>
2017-09-15 10:18:38 +0100

    turn off debug

--- dmlutgard/production_settings.py
+++ dmlutgard/production_settings.py
@@ -7,6 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
+DEBUG = False
 
 try:
 	SECRET_KEY = os.environ['SECRET_KEY']
