1a44af70802e77367db5ebbdb8672b9ba57b9187
yobmod <yobmod@gmail.com>
2018-01-24 22:30:14 +0000

    reorder local/prod 7

--- dmlutgard/production_settings.py
+++ dmlutgard/production_settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # DATABASES = {
 # 	'default': {
