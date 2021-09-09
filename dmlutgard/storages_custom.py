6c8239ad62515772f0b9e7ca599a3d42f7a3321c
yobmod <yobmod@gmail.com>
2021-09-09 18:18:00 +0100

    update boto3

--- dmlutgard/storages_custom.py
+++ dmlutgard/storages_custom.py
@@ -1,9 +1,8 @@
 from django.core.files.storage import get_storage_class
-from storages.backends.s3boto import S3BotoStorage
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
-# class CachedS3BotoStorage(S3BotoStorage):
+# class CachedS3BotoStorage(S3Boto3Storage):
 # 	"""	S3 storage backend that saves the files locally, too."""
 # 	def __init__(self, *args, **kwargs):
 # 		super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
@@ -15,13 +14,12 @@ from django.conf import settings
 # 		super(CachedS3BotoStorage, self).save(name, self.local_storage._open(name))
 # 		return name
 
-from storages.backends.s3boto import S3BotoStorage
 from compressor.storage import CompressorFileStorage
 from django.core.files.storage import get_storage_class
 import copy
 
 
-class CachedS3BotoStorage(S3BotoStorage):
+class CachedS3BotoStorage(S3Boto3Storage):
 	""" S3 storage backend that saves the files locally, too.  """
 	location = settings.STATICFILES_LOCATION
 
